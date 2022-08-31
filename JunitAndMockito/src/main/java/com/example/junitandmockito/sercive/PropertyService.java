96ea233973b59126b71ef54def2ab7e7bdc08e33
Qwerty <69114727+QwertySudo666@users.noreply.github.com>
2022-08-31 21:04:59 +0300

    Delete JunitAndMockito directory

--- JunitAndMockito/src/main/java/com/example/junitandmockito/sercive/PropertyService.java
+++
@@ -1,7 +0,0 @@
-package com.example.junitandmockito.sercive;
-
-import com.example.junitandmockito.dto.PropertyDTO;
-
-public interface PropertyService {
-    public PropertyDTO saveProperty(PropertyDTO propertyDTO);
-}
