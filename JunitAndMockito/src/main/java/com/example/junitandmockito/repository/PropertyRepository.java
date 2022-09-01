06c282584d9dcb6d67d6012c81183d9fdf943ffc
Qwerty <69114727+QwertySudo666@users.noreply.github.com>
2022-09-01 14:53:27 +0300

    Delete JunitAndMockito directory

--- JunitAndMockito/src/main/java/com/example/junitandmockito/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.junitandmockito.repository;
-
-import com.example.junitandmockito.entity.PropertyEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PropertyRepository extends CrudRepository<PropertyEntity, Long> {
-
-}
-
